c4cfb1ba01d5b596be6e03bf4f8f86ddc609c9e1
Parth Rastogi <rastogiparth2011@gmail.com>
2020-03-29 14:16:26 +0530

    Frontend

---
+++ Javascript/Basic Javascript Projects/Function Methods.js
@@ -0,0 +1,88 @@
+// TODO:: Function Object Methods :-
+
+/* 
+! Bind Method Explanation -
+? The bind() method creates a new function that, when called, has its this keyword set to the provided value,
+? with a given sequence of arguments preceding any provided when the new function is called.
+*/
+
+// ! bind() method Syntax -
+// * function_name.bind(this, arguments related to call function);
+
+// ! Bind Function Method Example :
+
+const module =
+{
+  x: 42,
+  getX: function () {
+    return this.x;
+  }
+}
+
+const unboundGetX = module.getX;
+console.log(unboundGetX());                    // ! The function gets invoked at the global scope
+// ! Expected Output: undefined
+
+const boundGetX = unboundGetX.bind(module);
+console.log(boundGetX());
+// ! Expected Output: 42
+
+// TODO =======================================================================================================================
+
+// ! Call Method Explanation -
+// ? The call() method calls a function with a given this value and arguments provided individually.
+
+// ! NOTE :-
+/*
+TODO:: While the syntax of this function is almost identical to that of apply(), 
+TODO:: the fundamental difference is that call() accepts an argument list, while apply() accepts a single array of arguments.
+*/
+
+// ! call() method Syntax -
+// * function_name.call(this, arguments related to call function);
+
+// ! Call Function Method Example :
+
+function Product(name, price)
+{
+  this.name = name;
+  this.price = price;
+}
+
+function Food(name, price)
+{
+  Product.call(this, name, price);
+  this.category = 'food';
+}
+
+console.log(new Food('cheese', 5).name);
+// ! Expected Output: "cheese"
+
+// TODO =============================================================================================================================
+
+// ! Apply Method Explanation -
+// ? The apply() method calls a function with a given this value, and arguments provided as an array (or an array-like object).
+
+// ! NOTE :-
+/*
+TODO:: apply() is very similar to call(), except for the type of arguments it supports. 
+TODO:: You use an arguments array instead of a list of arguments (parameters). With apply, you can also use an array literal,
+TODO:: for example, func.apply(this, ['eat', 'bananas']), or an Array object, for example, func.apply(this, new Array('eat', 'bananas')).
+*/
+
+// ! apply() method Syntax -
+// * function_name.apply(this, arguments array's);
+
+// ! Apply Function Method Example :
+
+const numbers = [5, 6, 2, 3, 7];
+
+const max = Math.max.apply(null, numbers);
+
+console.log(max);
+// ! Expected Output: 7
+
+const min = Math.min.apply(null, numbers);
+
+console.log(min);
+// ! Expected Output: 2
---
+++ Javascript/Frontend Projects/Movie Library Store/Movie.js
@@ -0,0 +1,170 @@
+// ! Global Variables :-
+
+const addMovieModal = document.querySelector("#add_modal");
+const deleteMovieModal = document.querySelector("#delete-modal");
+
+const startAddMovieButton = document.querySelector("header button");
+const backdrop = document.querySelector("#backdrop");
+const cancelAddMovieButton = document.querySelector(".btn--passive");
+const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
+
+const userInputs = document.querySelectorAll("input");
+const entryTextSection = document.querySelector("#entry-text");
+
+const movies = [];
+
+
+
+const toggleBackdrop = () => {
+  backdrop.classList.toggle("visible");
+};
+
+const updateUI = () =>
+{
+  if (movies.length === 0) 
+    entryTextSection.style.display = "block";
+  else 
+    entryTextSection.style.display = "none";
+};
+
+const closeMovieDeletionModal = () =>
+{
+  toggleBackdrop();
+  deleteMovieModal.classList.remove("visible");
+};
+
+const onDeleteMovie = movieId =>
+{
+  let movieIndex = 0;
+
+  for (const movie of movies)
+  {
+    if (movie.id === movieId) {
+      break;
+    }
+    movieIndex++;
+  }
+
+  movies.splice(movieIndex, 1);
+  
+  const listRoot = document.getElementById("movie-list");
+  
+  listRoot.children[movieIndex].remove();
+  // listRoot.removeChild(listRoot.children[movieIndex]);
+
+  closeMovieDeletionModal();
+  updateUI();
+};
+
+const onStartDeleteMovie = movieId =>
+{
+  deleteMovieModal.classList.add("visible");
+  toggleBackdrop();
+
+  const cancelDeletionButton = deleteMovieModal.querySelector(".btn--passive");
+  let confirmDeletionButton = deleteMovieModal.querySelector(".btn--danger");
+
+  // ! replaceWith() is used to replace the child nodes. 
+  // ! .cloneNode(true) is used to a existing node to another node to get the exact same value of the previous defined node.
+
+  confirmDeletionButton.replaceWith(confirmDeletionButton.cloneNode(true));
+
+  confirmDeletionButton = deleteMovieModal.querySelector(".btn--danger");
+
+  cancelDeletionButton.removeEventListener("click", closeMovieDeletionModal);
+
+  cancelDeletionButton.addEventListener("click", closeMovieDeletionModal);
+  confirmDeletionButton.addEventListener("click",onDeleteMovie.bind(null, movieId));
+};
+
+const renderNewMovieElement = (id, title, imageUrl, rating) =>
+{
+  const newMovieElement = document.createElement("li");
+
+  newMovieElement.className = "movie-element";
+  newMovieElement.innerHTML = `
+    <div class="movie-element__image">
+      <img src="${imageUrl}" alt="${title}">
+    </div>
+    <div class="movie-element__info">
+      <h2>${title}</h2>
+      <p>${rating}/5 stars</p>
+    </div>
+  `;
+
+  newMovieElement.addEventListener("click",onStartDeleteMovie.bind(null, id));
+  
+  const listRoot = document.getElementById("movie-list");
+  listRoot.appendChild(newMovieElement);
+};
+
+const closeMovieModal = () => {
+  addMovieModal.classList.remove("visible");
+};
+
+const showMovieModal = () =>
+{
+  addMovieModal.classList.add("visible");
+  toggleBackdrop();
+};
+
+const clearMovieInput = () =>
+{
+  for (const usrInput of userInputs) {
+    usrInput.value = "";
+  }
+};
+
+const onCancelAddMovie = () =>
+{
+  closeMovieModal();
+  toggleBackdrop();
+  clearMovieInput();
+};
+
+const onAddMovie = () =>
+{
+  const titleValue = userInputs[0].value;
+  const imageUrlValue = userInputs[1].value;
+  const ratingValue = userInputs[2].value;
+
+  // ! .trim() is an String Method.
+  // ! parseInt() can also be written as "+variable".
+
+  if (titleValue.trim() === ""|| imageUrlValue.trim() === ""|| ratingValue.trim() === ""|| parseInt(ratingValue) < 1 || parseInt(ratingValue) > 5)
+  {
+    alert("Please enter valid values (rating between 1 and 5).");
+    return;
+  }
+
+  const newMovie =
+  {
+    id: Math.random().toString(),
+    title: titleValue,
+    image: imageUrlValue,
+    rating: ratingValue
+  };
+
+  movies.push(newMovie);
+  console.log(movies);
+  
+  closeMovieModal();
+  toggleBackdrop();
+  clearMovieInput();
+  renderNewMovieElement(newMovie.id,newMovie.title,newMovie.image,newMovie.rating);
+  updateUI();
+};
+
+const onBackdropClick = () =>
+{
+  closeMovieModal();
+  closeMovieDeletionModal();
+  clearMovieInput();
+};
+
+// ! Event Listeners :-
+
+startAddMovieButton.addEventListener("click", showMovieModal);
+backdrop.addEventListener("click", onBackdropClick);
+cancelAddMovieButton.addEventListener("click", onCancelAddMovie);
+confirmAddMovieButton.addEventListener("click", onAddMovie);
