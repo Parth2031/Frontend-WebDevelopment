32bc9897b47d55de7008dee7a66f97334762800e
Parth Rastogi <rastogiparth2011@gmail.com>
2020-04-12 21:08:20 +0530

    Frontend

--- Javascript/JS Library's/Third Party Libraries/Lodash Library/Lodash.js
+++ Javascript/JS Library's/Third Party Libraries/Lodash Library/Lodash.js
@@ -11,3 +11,17 @@ const inactiveCustomers = _.difference(customers, activeCustomers);
 
 console.log("Difference of Array: ");
 console.log(inactiveCustomers);
+
+// ! _.uniqueId() is a method of Lodash Library which is used to generate unique Id and not repetive as it could be used be used instead of ....
+// ! .... Math.random()
+
+let n = Math.random();
+
+for (var i = 0; i < n; i++)
+{
+  var id1 = _.uniqueId();
+  var id2 = _.uniqueId();
+}
+
+console.log(id1);
+console.log(id2);
