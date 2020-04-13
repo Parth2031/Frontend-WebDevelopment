9cf17579c69bd12ef340d468f473a11c7cfe7589
Parth Rastogi <rastogiparth2011@gmail.com>
2020-04-13 18:27:02 +0530

    Frontend

---
+++ Javascript/Javascript Tooling/Webpack & ESLint/Modified Daily Planner Project/src/App/Component.js
@@ -0,0 +1,38 @@
+// TODO:: Types of "export" keyword :-
+/*
+? Named Exports(Zero or more exports per module)
+? Default Exports(One per module) - 
+!                                   "default keyword" is used.         
+*/
+
+// * This function is used as an example if a different function/class is exported without "default" keyword. 
+export function doSomething() { };
+
+// TODO:: "default" keyword is used only in "export" and is used when we want send one module per file.
+// ? NOTE - When we use "default" keyword, we don't require name of any class/function/variable,etc.
+
+export default class
+{
+  constructor(hostElementId, insertBefore = false)
+  {
+    if (hostElementId) 
+      this.hostElement = document.getElementById(hostElementId);
+    else 
+      this.hostElement = document.body;
+  
+    this.insertBefore = insertBefore;
+  }
+
+  detach()
+  {
+    if (this.element)
+    {
+      this.element.remove();
+      // this.element.parentElement.removeChild(this.element);
+    }
+  }
+
+  attach() {
+    this.hostElement.insertAdjacentElement(this.insertBefore ? 'afterbegin' : 'beforeend',this.element);
+  }
+}
---
+++ Javascript/Javascript Tooling/Webpack & ESLint/Modified Daily Planner Project/src/Utility/DOMHelper.js
@@ -0,0 +1,46 @@
+console.log('DOM Helper executing!');
+
+export class DOMHelper
+{
+  static clearEventListeners(element)
+  {
+    // ! Node.cloneNode(true) - It returns a duplicate of the node on which this method was called.
+    const clonedElement = element.cloneNode(true);
+
+    // ! ChildNode.replaceWith() - This method replaces this ChildNode in the children list of its parent with a set of Node or DOMString objects.
+    element.replaceWith(clonedElement);
+
+    return clonedElement;
+  }
+
+  static moveElement(elementId, newDestinationSelector)
+  {
+    const element = document.getElementById(elementId);
+    const destinationElement = document.querySelector(newDestinationSelector);
+  
+    destinationElement.append(element);
+
+    // ! The Element interface's scrollIntoView() method scrolls the element's parent container such that the element ....
+    // ! .... on which scrollIntoView() is called is visible to the user.
+    element.scrollIntoView({ behavior: 'smooth' });
+  }
+}
+
+// ? In this, the functions are exported and not the class so as to import them at when they are only needed in the Project.
+
+// export function clearEventListeners(element)
+// {
+//   const clonedElement = element.cloneNode(true);
+//   element.replaceWith(clonedElement);
+
+//   return clonedElement;
+// }
+
+// export function moveElement(elementId, newDestinationSelector)
+// {
+//   const element = document.getElementById(elementId);
+//   const destinationElement = document.querySelector(newDestinationSelector);
+//   destinationElement.append(element);
+
+//   element.scrollIntoView({ behavior: 'smooth' });
+// }
