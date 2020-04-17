d6e2fec7b65f025e026c5e5207e57a8efe417ba5
Parth Rastogi <rastogiparth2011@gmail.com>
2020-04-17 18:52:43 +0530

    Frontend

---
+++ Javascript/Javascript Concepts (ES6)/Meta Programming/Proxy.js
@@ -0,0 +1,40 @@
+// TODO:: Proxy API is used to define custom behavior for fundamental operations (e.g. property lookup, assignment, enumeration, 
+// TODO   function invocation, etc). Since, it is used as traps in object where you could catch the data and later be fetched.
+
+/*
+* NOTE : Proxy API containing get() & set() are getters and setter but with more flexibility as they are not bound to be worked 
+*        in a certain way only.
+? Syntax ->> const variable_name = new Proxy(target,handler);
+?            where, target -> object & handler -> propertyName.
+! Similar Built-In Methods to Reflect API.    
+*/
+
+// ! Example of Proxy API -
+
+const courseHandler =
+{
+  get(obj, propertyName)
+  {
+    console.log(propertyName);
+  
+    if (propertyName === 'length') 
+      return 0;
+
+    return obj[propertyName] || 'NOT FOUND';
+  },
+
+  set(obj, propertyName, newValue)
+  {
+    console.log('Sending data ...');
+  
+    if (propertyName === 'rating') 
+      return;
+    
+    obj[propertyName] = newValue;
+  }
+};
+
+const pCourse = new Proxy(course, courseHandler);
+pCourse.rating = 5;
+
+console.log(pCourse.title, pCourse.length, pCourse.rating);
