710f62b14ae1ebe7b639e27cc79899b5a5562250
Parth Rastogi <rastogiparth2011@gmail.com>
2020-04-19 22:48:34 +0530

    Frontend

--- React/Setup.js
+++ React/Setup.js
@@ -5,6 +5,7 @@
 
 ? React is a Javascript Library for building user interface. User Interface are Components which are Custom HTML Elements.
 
+
 ? React Components can be created by two ways -> 
 * 1st way : Using App.js file as the Main File but is used to create small projects as it could create confusion.
 * 2nd way : Creating .jsx files as it works like Modular Javascript for handling multiple files using import and export keywords.
