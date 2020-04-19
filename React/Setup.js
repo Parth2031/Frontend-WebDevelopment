504737f825159ede7f54ba9df35cef7b98ea283c
Parth Rastogi <rastogiparth2011@gmail.com>
2020-04-19 22:42:55 +0530

    Frontend

--- React/Setup.js
+++ React/Setup.js
@@ -4,11 +4,19 @@
 ! Explanation of React Terms - 
 
 ? React is a Javascript Library for building user interface. User Interface are Components which are Custom HTML Elements.
-? 
+
+? React Components can be created by two ways -> 
+* 1st way : Using App.js file as the Main File but is used to create small projects as it could create confusion.
+* 2nd way : Creating .jsx files as it works like Modular Javascript for handling multiple files using import and export keywords.
+
+! NOTE on JSX (Javascript Extension) Files ->>
+
+? Since JSX is closer to JavaScript than to HTML, React DOM uses camelCase property naming convention instead of HTML attribute names.
+? For example, class becomes className in JSX, and tabindex becomes tabIndex.
 
 */
 
-// TODO:: npm Commands (Terminal Commands) in order to install react app :-
+// TODO:: npm Commands (Terminal Commands) in order to install React App :-
 
 /*
 ! React Setup (Terminal Commands) -
@@ -19,11 +27,5 @@
 * Navigate to the created reaact app folder_name -> cd folder_name/
 ? npm run start : It is to run the Development Server.
 ? npm run build : It is a create a production build.
-*/
-
-/*
-TODO:: React App Folder's working -
-
-? App.js in src folder is the main file where, in 'div classname = "App"' we write the code logic. 
 */
 
