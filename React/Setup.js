6ea88122693b5cf71409b99396afa164195f16c8
Parth Rastogi <rastogiparth2011@gmail.com>
2020-04-19 20:09:07 +0530

    Frontend

---
+++ React/Setup.js
@@ -0,0 +1,29 @@
+// TODO:: Understanding React Setup, Syntax & Terms :-
+
+/*
+! Explanation of React Terms - 
+
+? React is a Javascript Library for building user interface. User Interface are Components which are Custom HTML Elements.
+? 
+
+*/
+
+// TODO:: npm Commands (Terminal Commands) in order to install react app :-
+
+/*
+! React Setup (Terminal Commands) -
+
+? npm install create-react-app -g : It is to globally install the Create React App Library which holds all React Dependencies.
+? create-react-app anyfolder_name : It is to create a folder containing the React App Dependencies in that folder.
+
+* Navigate to the created reaact app folder_name -> cd folder_name/
+? npm run start : It is to run the Development Server.
+? npm run build : It is a create a production build.
+*/
+
+/*
+TODO:: React App Folder's working -
+
+? App.js in src folder is the main file where, in 'div classname = "App"' we write the code logic. 
+*/
+
