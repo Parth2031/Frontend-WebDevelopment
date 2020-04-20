bcb83a90a4f8c0b0f27013e0350c6003f901e76f
Parth Rastogi <rastogiparth2011@gmail.com>
2020-04-20 20:36:56 +0530

    Frontend

--- React/understanding-react-setup/src/Components/Person.jsx
+++ React/understanding-react-setup/src/Components/Person.jsx
@@ -8,11 +8,14 @@ import './Person.css'
 ? We will recieve the Attributes from the Call Function/Component Name Tag as a single argument(props) ....
 ? ....  which could be used further as by properties name in the Tag.
 
-TODO:: Methods & Properties Used for Event Listenener ->
+TODO:: Methods & Properties Used for Event Listenener Attributes ->
 
 * .children - It is a keyword in React which retrieves the Text written/Any Other Tag between the Component Name Tag.
 ? onClick = {props.attribute_name} is a Method Reference by App.js through which we can also execute same thing by props rather that state keyword.
 ? onChange = {props.attribute_name} is a Method that changes value as output as soon as we change it manually.
+
+! NOTE ->> When using a Event Listener Attribute in .jsx files, It is called Passing Method Reference.
+!          onChange Attribute is a type of Two Way Binding.  
 */
 
 const person = (props) =>
