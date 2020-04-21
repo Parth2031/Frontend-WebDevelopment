4088e250e100ad4cb149d062d4a42b0155b4e775
Parth Rastogi <rastogiparth2011@gmail.com>
2020-04-21 20:16:43 +0530

    Frontend

--- React/understanding-react-setup/src/App.js
+++ React/understanding-react-setup/src/App.js
@@ -9,29 +9,125 @@ import React,{ Component } from 'react';
 import './App.css';
 import Person from './Components/Person';
 
+// TODO:: Main Core Concepts Working :-
+
+// class App extends Component
+// {
+//   // ! Syntax of React -
+//   // ? return( <div className="App"> Any More HTML Elements like HTML File </div>);
+
+//   // * NOTE - A state is a reserved keyword in App.js file which is used when ....
+//   // *        .... a component needs state when some data associated with it changes over time.
+
+//   /*
+//   ! Difference between props(.jsx) and state(App.js) -
+
+//   ? It is that props are passed from a parent component, but state is managed by the component itself. 
+//   ? A component cannot change its props, but it can change its state.
+//   */
+
+//   state =
+//   {
+//     persons: [
+//       { name: "Nitya", age: 20 },
+//       { name: "Tanya", age: 20 },
+//       { name: "Christina", age: 19 }
+//     ],
+//     otherState: "Some Value"
+//   }
+
+//   switchNameHandler = (newName) =>
+//   {
+//     this.setState
+//     ({
+//       persons: [
+//         { name: newName, age: 20 },
+//         { name: "Harshit", age: 20 },
+//         { name: "Mrinal", age: 20 }        
+//       ]
+//     }); 
+//   }
+
+//   nameChangedHandler = (event) =>
+//   {
+//     this.setState
+//     ({
+//       persons: [
+//         { name: "Parth", age: 20 },
+//         { name: event.target.value, age: 20 },
+//         { name: "Mrinal", age: 20 }        
+//       ]
+//     }); 
+//   }
+
+//   render()
+//   {
+//     const inlineStyle =
+//     {
+//       backgroundColor: 'white',
+//       font: 'inherit',
+//       border: '1px solid blue',
+//       padding: '8px',
+//       cursor: 'pointer'
+//     }
+
+//    /*
+//    TODO:: Some NOTES on the Return Statement Portion -->>  
+   
+//    ? onClick is the property here,to use Event Listener and calling a method without () at end will only send a reference. .....
+//    ?  .... And not immediately running the event as soon as file loads. 
+
+//    * There are two ways of Dynamic Object Name Reference ->>
+  
+//    ? 1st way : <button onClick={this.switchNameHandler.bind(this, "Parth")} style={inlineStyle}>Switch Name</button>
+//    ? 2nd way : <button onClick={() => this.switchNameHandler("Parth")}>Switch Name</button>
+
+//    * 2nd way can be inefficient sometimes according to React Working.
+  
+//    ! Note -> It is the way to call a .jsx file in the Main File, if we don't want any thing inside it, else we could do the normal way also.
+//    ?         Example -> <Person/> 
+//    */
+
+//     return (
+//       <div className="App">
+//         <h1>Understanding Basic React</h1>
+
+//         {/* <button onClick={this.switchNameHandler}>Switch Name</button> */}
+
+//         <button onClick={this.switchNameHandler.bind(this, "Parth")} style={inlineStyle}>Switch Name</button>
+//         {/* <button onClick={() => this.switchNameHandler("Parth")}>Switch Name</button> */}
+     
+//         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} click={this.switchNameHandler.bind(this,"Shrey")}/>
+//         <Person name={this.state.persons[1].name} age={this.state.persons[1].age} changed={this.nameChangedHandler}/>
+//         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+                
+//         <Person name="Utkarsh" age="19">My Hobbies: Coding</Person>
+//       </div>
+//     );
+//   }
+
+//   // ! Alternative Syntax of React -
+//   // ? return React.createElement('html element',configuration,'string of text');
+
+//   // return React.createElement('div', { className: "App" }, React.createElement('h1',null, 'Hi,I am free')); 
+// }
+
+// export default App;
+
+// // ---------------------------------------------------------------------------------------------------------------------------------------
+
+// TODO:: Advanced Core Concepts Working :-
+
 class App extends Component
 {
-  // ! Syntax of React -
-  // ? return( <div className="App"> Any More HTML Elements like HTML File </div>);
-
-  // * NOTE - A state is a reserved keyword in App.js file which is used when ....
-  // *        .... a component needs state when some data associated with it changes over time.
-
-  /*
-  ! Difference between props(.jsx) and state(App.js) -
-
-  ? It is that props are passed from a parent component, but state is managed by the component itself. 
-  ? A component cannot change its props, but it can change its state.
-  */
-
   state =
   {
     persons: [
-      { name: "Nitya", age: 20 },
-      { name: "Tanya", age: 20 },
-      { name: "Christina", age: 19 }
+      { id: 1, name: "Nitya", age: 20 },
+      { id: 2, name: "Tanya", age: 20 },
+      { id: 3, name: "Christina", age: 19 }
     ],
-    otherState: "Some Value"
+    showPersons: false
   }
 
   switchNameHandler = (newName) =>
@@ -46,16 +142,37 @@ class App extends Component
     }); 
   }
 
-  nameChangedHandler = (event) =>
+  // ! NOTE -> In this, we updated the function using reference concepts to update value for all input text box.
+
+  nameChangedHandler = (event, id) =>
   {
-    this.setState
-    ({
-      persons: [
-        { name: "Parth", age: 20 },
-        { name: event.target.value, age: 20 },
-        { name: "Mrinal", age: 20 }        
-      ]
-    }); 
+    const personIndex = this.state.persons.findIndex(p => p.id === id);
+
+    const person = { ...this.state.persons[personIndex] };
+    person.name = event.target.value;
+
+    const people = [...this.state.persons];
+    people[personIndex] = person;
+
+    this.setState({ persons: people }); 
+  }
+  
+  togglePersonsHandler = () =>
+  {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  }
+
+  deletePersonHandler = (personIndex) =>
+  {
+    // ! In this, it creates a copy of the array and not create changes on the original array.
+    // const persons = this.state.persons.slice();
+
+    // ! Alternative Way to slice() method ->
+    const persons = [...this.state.persons];
+    
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
   }
 
   render()
@@ -69,45 +186,71 @@ class App extends Component
       cursor: 'pointer'
     }
 
-   /*
-   TODO:: Some NOTES on the Return Statement Portion -->>  
-   
-   ? onClick is the property here,to use Event Listener and calling a method without () at end will only send a reference. .....
-   ?  .... And not immediately running the event as soon as file loads. 
+    let persons = null;
 
-   * There are two ways of Dynamic Object Name Reference ->>
-  
-   ? 1st way : <button onClick={this.switchNameHandler.bind(this, "Parth")} style={inlineStyle}>Switch Name</button>
-   ? 2nd way : <button onClick={() => this.switchNameHandler("Parth")}>Switch Name</button>
+    // ! NOTE -->> It shows as an Alternative Way to Ternary Opertor and can use the Conditions :-
 
-   * 2nd way can be inefficient sometimes according to React Working.
-  
-   ! Note -> It is the way to call a .jsx file in the Main File, if we don't want any thing inside it, else we could do the normal way also.
-   ?         Example -> <Person/> 
-   */
+    // if (this.state.showPersons) 
+    // {
+    //   persons = (
+    //     <div>
+    //       <Person name={this.state.persons[0].name} age={this.state.persons[0].age} click={this.switchNameHandler.bind(this, "Shrey")} />
+    //       <Person name={this.state.persons[1].name} age={this.state.persons[1].age} changed={this.nameChangedHandler} />
+    //       <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+    //     </div> 
+    //   );
+    // }  
+
+    
+    if (this.state.showPersons) 
+    {
+      persons = (
+        <div>
+          {
+            this.state.persons.map((person, index) =>
+            {
+              return <Person
+                click={() => this.deletePersonHandler(index)}
+                name={person.name}
+                age={person.age}
+                key={person.id}            // ! <-- Key Property is used to Uniquely Identify Objects Value by using some sort of ID.
+                changed={(event) => this.nameChangedHandler(event,person.id)} />   
+            })
+          }
+        </div> 
+      );
+    }  
+
+    /*
+     TODO:: Some NOTES on return statement -->>
+
+     * Below Syntax shows that it looks like HTML but it is actually in Javascript so, we use Curly Braces as we could put Conditions 
+     ! NOTE-> But we cannot use if else statements in return statement but only ternary opertor on some condition.
+     ?  { Condition ? JSX Code : null } 
+
+     
+    */
 
     return (
       <div className="App">
         <h1>Understanding Basic React</h1>
+        
+        <button onClick={this.togglePersonsHandler} style={inlineStyle}>Toggle Person's</button>
+         
+        {/* {
+          this.state.showPersons === true ?  
+           <div>
+            <Person name={this.state.persons[0].name} age={this.state.persons[0].age} click={this.switchNameHandler.bind(this, "Shrey")} />
+            <Person name={this.state.persons[1].name} age={this.state.persons[1].age} changed={this.nameChangedHandler} />
+            <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+           </div> : null
+        } */}
 
-        {/* <button onClick={this.switchNameHandler}>Switch Name</button> */}
-
-        <button onClick={this.switchNameHandler.bind(this, "Parth")} style={inlineStyle}>Switch Name</button>
-        {/* <button onClick={() => this.switchNameHandler("Parth")}>Switch Name</button> */}
-     
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} click={this.switchNameHandler.bind(this,"Shrey")}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} changed={this.nameChangedHandler}/>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
-                
-        <Person name="Utkarsh" age="19">My Hobbies: Coding</Person>
+        {/* Alternative to Above Method */}
+        {persons}
       </div>
     );
   }
-
-  // ! Alternative Syntax of React -
-  // ? return React.createElement('html element',configuration,'string of text');
-
-  // return React.createElement('div', { className: "App" }, React.createElement('h1',null, 'Hi,I am free')); 
 }
 
 export default App;
