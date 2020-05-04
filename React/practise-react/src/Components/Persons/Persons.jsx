4f952bac99798dae698ebcc38a484d5e6e364de7
Parth Rastogi <rastogiparth2011@gmail.com>
2020-05-04 14:23:48 +0530

    Frontend

---
+++ React/practise-react/src/Components/Persons/Persons.jsx
@@ -0,0 +1,62 @@
+// TODO:: Understanding LifeCycle Hooks Working for React Setup/Working.
+
+import React, { Component } from 'react';
+import Person from './Person/Person';
+
+class Persons extends Component
+{
+  // ! LifeCycle Hooks Woking but not used in Main React Setup Project. 
+
+  // static getDerivedStateFromProps(props, state) {
+  //   console.log('[Persons.js] getDerivedStateFromProps');
+  //   return state;
+  // }
+
+  // componentWillReceiveProps(props) {
+  //   console.log('[Persons.js] componentWillReceiveProps', props);
+  // }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[Persons.js] shouldComponentUpdate');
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('[Persons.js] getSnapshotBeforeUpdate');
+    return { message: 'Snapshot!' };
+  }
+
+  // componentWillUpdate() {
+
+  // }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log('[Persons.js] componentDidUpdate');
+    console.log(snapshot);
+  }
+
+  componentWillUnmount() {
+    console.log('[Persons.js] componentWillUnmount');
+  }
+
+  // // ------------------------------------------------------------------------------------------------------------------------
+
+  render()
+  {
+    console.log('[Persons.js] rendering...');
+  
+    return this.props.persons.map((person, index) => {
+      return (
+        <Person
+          click={() => this.props.clicked(index)}
+          name={person.name}
+          age={person.age}
+          key={person.id}
+          changed={event => this.props.changed(event, person.id)}
+        />
+      );
+    });
+  }
+}
+
+export default Persons;
