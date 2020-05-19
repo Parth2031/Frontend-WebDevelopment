f8cf32ca95b42d3313fd412ee2aa7281d6bc0112
Parth Rastogi <rastogiparth2011@gmail.com>
2020-05-19 21:24:42 +0530

    Frontend

--- Typescript/Typescript.js
+++ Typescript/Typescript.js
@@ -16,7 +16,7 @@
 
 TODO:: Terminal Commands for executing Typescript ->>
 
-? tsc : It compiles Typescript to Javascript.
+? tsc : It compiles all Typescript files to Javascript files.
 ? tsc --init : It is used to set the congiguration of Typescript by getting tsconfig.json.
 */
 
@@ -26,7 +26,7 @@ TODO:: Terminal Commands for executing Typescript ->>
 
 /*
 * NOTE :- In Typescript, when we create a variable using any var,let,const.
-*         When we intially set a value to that variable which Typescript set's itself according to it and not always.
+*         When we intially set a value to that variable which Typescript set's itself according to it but not at only defining stage.
 *         It means when we create a variable with number value then, we can't assign it with a string/boolean values and vice versa.
 
 ! Example of a variable defined with intially a String Value :
@@ -47,15 +47,27 @@ TODO:: Syntax :- variable variable_name: type;
 ? let myName: string;
 ? myName = 'Parth';
 
+TODO:: ANY Data Type :->
+
+* NOTE :- "Any" Data Type is used to set the Data Type similar to Javascript Behaviour as we can change it's data type anytime.
+
+! Syntax :- variable variable_name: any = value;
+!           where, value can be number,string,boolean,object,etc.
+
+? Example : let car: any = "BMW";
+?           console.log(car);
+?           car = { brand: "BMW", series: 3 };
+?           console.log(car);
 
 TODO:: Similary, for Array Data Types ->>
 
-* NOTE :- We can define array of string,number,etc at once by default. So, to change the Array Data Types explicitly.
+* NOTE :- We can define array of string[],number[],etc at once by default. So, to change the Array Data Types explicitly.
 
 ! Example of Array set as Explicitly and not defined as Intially :
 
 ? let hobbies: any[] = ["Cooking","Dancing"];
-? hobbies = [100];
+? hobbies = [100];                                    // ! Without "any[]", it would have been incorrect type assigning.
+? console.log(hobbies);
 
 * NOTE :- But, when we set Array Dta Type as "any[]", it means that the last value type is the Set Data Type of that Array.
 
@@ -63,7 +75,7 @@ TODO:: Similary, for Array Data Types ->>
 TODO:: Tuples (Type of Array) Data Type :->
 
 ! Tuples is a mixed Array Data Type means that an Array containing various Data Types at a single time.
-* Syntax :- variable variable_name: [data types in a set order,..] = [values according to specified data types,..];
+* Syntax :- variable array_name: [data types in a set order,..] = [values according to specified data types,..];
 
 ? Example : let address: [string, number] = ["Patparganj",85];
 
@@ -80,6 +92,8 @@ TODO:             value_2 = 100,  --> 100
 TODO:             value_3       --> 101                // ? All of the following members are auto-incremented from that point on.
 TODO:           }
 
+TODO::          let another_variable_name: enum_variable_name = enum_variable_name.value_1;
+
 * NOTE :-  In a string enum, each member has to be constant-initialized with a string literal, or with another string enum member.
 
 ? Example : enum Direction
@@ -89,8 +103,81 @@ TODO:           }
 ?             Left = "LEFT",
 ?             Right = "RIGHT",
 ?           }
+
+?           let directions: Direction = Direction.Up;
 */
 
 // // ----------------------------------------------------------------------------------------------------------------------------------
 
+// TODO:: Understanding Typescript Functions ->
 
+/*
+* NOTE :- We can also assign data types to Functions and it's Arguments like number,string,etc.
+*         But, we also assign special types like "void". As "void" doesn't allow functions to return any value.
+
+* Order of types is very important in function.
+
+! Syntax :- function function_name(argument_name: type,..) : type {
+!             return ....;
+!           }
+
+? Example : function warnUser(): void {
+?             console.log("This is my Warning Message");
+?            }
+
+TODO:: Function Types ->>
+
+* So, in this basically, we are setting a function declaration and not actually writing function definition.
+! Syntax :- function function_name: (argument_name: type,argument_name: type) => type;
+
+? Example : function multiply(valueOne: number, valueTwo: number) {
+?             return valueOne * valueTwo;
+?           }
+?           let myMultiply: (a: number, b: number) => number;                // ! <-- Function Declaration
+?           myMultiply = multiply;
+?           console.log(myMultiply(4, 5));
+
+?           myMultiply = warnUser;                                 // ! <-- So, It is incorrect due to Function Declaration
+?           myMultiply();
+*/
+
+// // ----------------------------------------------------------------------------------------------------------------------------------
+
+// TODO:: Understanding Typescript Objects ->
+
+/*
+* NOTE :- In Objects, Object type is not important but it's key name & types are important. So, we cannot change key_name everytime.
+
+! Syntax :- variable object_name: {key_name: type, key_name: type}
+!           {
+!             key_name: value,
+!             .....
+!           }
+
+? Example : let userData: { name: string, age: number } =
+?           {
+?             name: "Parth",
+?             age: 20
+?           };
+?           console.log(userData);
+
+TODO:: Understanding Type Alias :-
+
+* NOTE :- Type Alias is basically storing a object declaration in a variable so that it can be reused multiple times.
+*         "type" is the Keyword Here.
+! Syntax :- type variable_name = { key_name: type, key_name: type};
+
+? Example : type complexDeclaration = { data: number[], output: (all: boolean) => number[] };
+
+! Complex Object Working ->>
+
+? Example : let complex: complexDeclaration =
+?           {
+?             data: [100, 20, 50],
+?             output:
+?                     function (all: boolean) {
+?                       return this.data;
+?                     }
+?           }
+?           console.log(complex.output(true));
+*/
