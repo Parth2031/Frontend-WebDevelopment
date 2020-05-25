092f5dedfa4b9c5d30ab4dadd430f0b0177576b5
Parth Rastogi <rastogiparth2011@gmail.com>
2020-05-25 21:56:48 +0530

    Frontend

--- Typescript/Modular Typescript/ES6 Modules & Libraries/Maths/Circle.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.calculateCirumcumference = exports.PI = void 0;
-exports.PI = 3.14;
-function calculateCirumcumference(diameter) {
-    return diameter * exports.PI;
-}
-exports.calculateCirumcumference = calculateCirumcumference;
