722deb95f7501c084382c46c95515ad9aa5b2582
Parth Rastogi <rastogiparth2011@gmail.com>
2020-05-25 21:57:21 +0530

    Frontend

---
+++ Typescript/Modular Typescript/ES6 Modules & Libraries/Modules.ts
@@ -0,0 +1,24 @@
+// TODO:: Typescript Modules is similar ( Same ) to Javascript ES6 Modules :-
+// ? But it just requires, Module Loader which is SysemJS ( A Third Party Library ).
+
+import * as Circle from "./Maths/Circle";
+import area from "./Maths/Rectangles";
+
+console.log(Circle.PI);
+console.log(Circle.calculateCirumcumference(10));
+console.log(area(20, 50));
+
+// // --------------------------------------------------------------------------------------------------------------------------------
+
+// TODO:: Working of a Third Party Library :-
+
+import "lodash";
+import _ from "lodash";
+
+const customers = ['Max', 'Manuel', 'Anna'];
+const activeCustomers = ['Max', 'Manuel'];
+
+const inactiveCustomers = _.difference(customers, activeCustomers);
+
+console.log("Difference of Array: ");
+console.log(inactiveCustomers);
