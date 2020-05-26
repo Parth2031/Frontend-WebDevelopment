edc1099a484bc2c7cf9421ea200a4a09108322f8
Parth Rastogi <rastogiparth2011@gmail.com>
2020-05-26 21:47:37 +0530

    Frontend

---
+++ React/burger-builder-ordering/src/index.js
@@ -0,0 +1,31 @@
+// TODO:: Setting up React Routing in Global File so as to access Routing to the directly linked files.
+// ? Setting Up React Redux in Global File in order to Manage Global State Management.
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import Reducer from '../src/Store/Reducer';
+
+const store = createStore(Reducer); 
+
+const app = (
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>  
+);
+
+ReactDOM.render(app ,  document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+
+serviceWorker.unregister();
