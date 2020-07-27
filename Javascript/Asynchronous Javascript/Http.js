24aaffcb58678f18843a3d848bc83540e40c7f36
Parth Rastogi <rastogiparth2011@gmail.com>
2020-07-27 20:21:02 +0530

    Frontend

--- Javascript/Asynchronous Javascript/Http.js
+++ Javascript/Asynchronous Javascript/Http.js
@@ -3,7 +3,7 @@
 // * NOTE :- Difference beteen Http Request and REST API :
 
 /*
-? REST API (Represential State Transfer API) which mainly depends on HTTP Request.
+? REST API (Represential State Transfer API) which mainly depends on HTTP Request but can be used on other Protocols as well.
 
   HTTP is a communications protocol that transports messages over a network. 
   SOAP is a protocol to exchange XML-based messages that can use HTTP to transport those messages. 
@@ -14,14 +14,14 @@
 // ! Types of Http Request :-
 
 // ? GET Request - It retrieves data from a specified resource(API).
-// ? POST Request - It is used to submit data to be processed to a specified resource(API).
-// ? UPDATE Request - It updates a specified resource(API).
-// ? DELETE Request - It deletes a specified resource(API).
+// ? POST Request - It is used create and submit data to be processed to a specified resource(API).
+// ? UPDATE Request - It updates a specified resource pf (API).
+// ? DELETE Request - It deletes a specified resource of (API).
 // ? PUT Request - It creates a new resource or replaces a representation of the target resource with the request payload.
 
-// ? HEAD Request - It is same as GET Request but does not a return a body.
-// ? OPTION Request - It return the supported http methods.
-// ? PATCH Request - It updates the partial resources.
+// ? HEAD Request - It is same as GET Request but does not a return a body and gets reponse headers from resource.
+// ? OPTION Request - It return the supported http methods and gets the option available from this resource.
+// ? PATCH Request - It updates and modifies the partial resources of (API).
 
 // * NOTE - Difference between POST & PUT Request :
 
@@ -34,9 +34,12 @@
 // ! HTTP Statuses :-
 
 // ? 200: "OK"
-// ? 201-300: "OK and Resource  is creted"
+// ? 201-300: "OK and Resource  is created"
+// ? 401: "Unauthorized"
 // ? 403: "Forbidden"
 // ? 404: "Not Found"
+// ? 405: "Method Not Allowed"
+// ? 409: "Conflict"
 
 // ! Other Types of Request - 
 
